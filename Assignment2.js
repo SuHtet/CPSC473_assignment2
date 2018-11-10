1d903a3f71aeba9c4a3c98288dace42faabd13f4
SuHtet <suhtet@csu.fullerton.edu>
2018-11-09 20:09:28 -0800

    Update Assignment2.js

--- Assignment2.js
+++ Assignment2.js
@@ -1,10 +1,14 @@
+// Student object
+
 var myObj = {
     "student": [
         {"firstname":"John", "lastname":"Smith", "Courses":["CPSC121 : A" , "CPSC131 : B+"]},
-        {"firstname":"James", "lastname":"Tran", "Courses":["CPSC21 : A+" , "CPSC131 : B+"]},
-        {"firstname":"Gio", "lastname":"Gonzalez", "Courses":["CPSC121 : A-" , "CPSC131 : B+"]}
+        {"firstname":"Gio", "lastname":"Gonzalez", "Courses":["CPSC121 : A+" , "CPSC131 : B+"]},
+        {"firstname":"James", "lastname":"Tran", "Courses":["CPSC121 : A-" , "CPSC131 : B+"]}
     ]
 }
+
+//Function to search students by their last LastName
 
 function myTestFunction() {
   var found = false;
@@ -13,6 +17,7 @@ function myTestFunction() {
 
 var searchName = document.getElementById("searchName").value;
 
+//loop to go through all the students in the array
 for (i = 0; i < myObj.student.length; i++){
     if (searchName == myObj.student[i].lastname) {
       foundList.push(myObj.student[i].firstname + " " + myObj.student[i].lastname + "<br/>" );
@@ -23,6 +28,7 @@ for (i = 0; i < myObj.student.length; i++){
 document.getElementById("studentsFound").innerHTML = foundList;
 }
 
+// To print out student names and their grades
 function printO(){
   var searchedName = document.getElementById("studentsFound").innerHTML;
   document.getElementById("gradesTitle").innerHTML = "Grades for " + " "+ searchedName;
